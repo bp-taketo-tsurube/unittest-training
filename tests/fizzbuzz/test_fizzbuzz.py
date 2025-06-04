c6a930a3f5de33194bb9ec421d93349708a1ceb6
bp-taketo-tsurube <taketo.tsurube@brainpad.co.jp>
2025-06-04 04:48:49 +0000

    add fizzbuzz test

--- tests/fizzbuzz/test_fizzbuzz.py
+++ tests/fizzbuzz/test_fizzbuzz.py
@@ -1,6 +1,8 @@
 from fizzbuzz.fizzbuzz import fizzbuzz
+import pytest
 
 
+# 3の倍数「Fizz」
 def test_fizzbuzz_example():
     # Arrange
     input_value = 3
@@ -11,3 +13,86 @@ def test_fizzbuzz_example():
 
     # Assert
     assert result == expected_output
+
+
+# 5の倍数「Buzz」
+def test_fizzbuzz_five():
+    # Arrange
+    input_value = 5
+    expected_output = "Buzz"
+
+    # Act
+    result = fizzbuzz(input_value)
+
+    # Assert
+    assert result == expected_output
+
+
+# n15
+def test_fizzbuzz_15():
+    # Arrange
+    input_value = 15
+    expected_output = "FizzBuzz"
+
+    # Act
+    result = fizzbuzz(input_value)
+
+    # Assert
+    assert result == expected_output
+
+
+# nnot 3 5
+def test_fizzbuzz_7():
+    # Arrange
+    input_value = 7
+    expected_output = "7"
+
+    # Act
+    result = fizzbuzz(input_value)
+
+    # Assert
+    assert result == expected_output
+
+
+def test_fizzbuzz_0():
+    # Arrange
+    input_value = 0
+    # expected_output = ValueError
+
+    # Act
+    with pytest.raises(ValueError) as e:
+        result = fizzbuzz(input_value)
+
+    # Assert
+    assert str(e.value) == "n must be between 1 and 1000."
+
+
+def test_fizzbuzz_1001():
+    # Arrange
+    input_value = 1001
+    # expected_output = ValueError
+
+    # Act
+    with pytest.raises(ValueError) as e:
+        result = fizzbuzz(input_value)
+
+    # Assert
+    assert str(e.value) == "n must be between 1 and 1000."
+
+
+def test_fizzbuzz_str():
+    # Arrange
+    input_value = "a"
+    # expected_output = ValueError
+
+    # Act
+    with pytest.raises(TypeError) as e:
+        result = fizzbuzz(input_value)
+
+    # Assert
+    assert str(e.value) == "n must be an integer."
+
+
+# n<1
+# n>1000
+# n not integer
